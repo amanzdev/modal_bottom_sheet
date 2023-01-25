57587fc0d07402c2c31e749283c24ff836b92e40
Haikal Izzuddin <haikalizz05@gmail.com>
2023-01-25 23:23:29 +0800

    Use false for shouldIgnorePoint when using BallisticScrollActivity

--- sheet/lib/src/scroll_controller.dart
+++ sheet/lib/src/scroll_controller.dart
@@ -179,6 +179,7 @@ class SheetPrimaryScrollPosition extends ScrollPositionWithSingleContext {
             velocity,
           ),
           context.vsync,
+          false,
         ),
       );
       return;
